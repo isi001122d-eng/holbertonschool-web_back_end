37fd469b88406cfea91bafcf24f3fbf7f1d99c46
isi001122d-eng <isi001122d@gmail.com>
2026-05-16 19:20:38 +0400

    Update users.py

--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -19,8 +19,6 @@ def view_all_users() -> str:
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
-    Path parameter:
-      - User ID
     Return:
       - User object JSON represented
       - 404 if the User ID doesn't exist
@@ -33,11 +31,10 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id: str = None) -> str:
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_one_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
-    Path parameter:
-      - User ID
     Return:
       - empty JSON is the User has been correctly deleted
       - 404 if the User ID doesn't exist
@@ -54,11 +51,6 @@ def delete_user(user_id: str = None) -> str:
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
     """ POST /api/v1/users/
-    JSON body:
-      - email
-      - password
-      - last_name (optional)
-      - first_name (optional)
     Return:
       - User object JSON represented
       - 400 if can't create the new User
@@ -66,7 +58,7 @@ def create_user() -> str:
     rj = None
     error_msg = None
     try:
-        rj = request.get_json()
+        rj = request.get_json(silent=True)
     except Exception as e:
         rj = None
     if rj is None:
@@ -90,13 +82,8 @@ def create_user() -> str:
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id: str = None) -> str:
+def update_one_user(user_id: str = None) -> str:
     """ PUT /api/v1/users/:id
-    Path parameter:
-      - User ID
-    JSON body:
-      - last_name (optional)
-      - first_name (optional)
     Return:
       - User object JSON represented
       - 404 if the User ID doesn't exist
@@ -109,7 +96,7 @@ def update_user(user_id: str = None) -> str:
         abort(404)
     rj = None
     try:
-        rj = request.get_json()
+        rj = request.get_json(silent=True)
     except Exception as e:
         rj = None
     if rj is None:
